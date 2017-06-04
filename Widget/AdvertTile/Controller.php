63dd091c4798463a43c24f3a752c0bbe93669e8a
Fredrik F Lindhagen <fred.lindh96@gmail.com>
2017-06-04 16:26:29 +0200

    Initial commit

---
+++ Widget/AdvertTile/Controller.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Plugin\AdvertTile\Widget\AdvertTile;
+
+class Controller extends \Ip\WidgetController
+{
+
+    public function generateHtml($revisionId, $widgetId, $data, $skin)
+    {
+        return parent::generateHtml($revisionId, $widgetId, $data, $skin);
+    }
+}
